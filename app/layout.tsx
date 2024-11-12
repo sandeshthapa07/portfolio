5fd4be38b5c7189cbbc74cbcce06477fdc4dbc2a
Sandesh Thapa <sandesh.thapa@aitc.ai>
2024-11-12 11:22:47 +0545

    feat: added made

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
-import Link from "next/link";
 
 import { Inter } from "next/font/google";
 
@@ -21,11 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}  flex flex-row  antialiased`}>
-        <aside className="scrollable-area lg:w-60 xl:w-72 h-full bg-white text-black border-r border-gray-200 flex flex-col p-3  ">
+        {/* <aside className="scrollable-area lg:w-60 xl:w-72 h-full bg-white text-black border-r border-gray-200 flex flex-col p-3  ">
           <div className="flex flex-row gap-1"></div>
           <Link href="/blog">Blog</Link>
           <Link href="/theme-changer">Theme Changer</Link>
-        </aside>
+        </aside> */}
         <main className="scrollable-area  flex-1">{children}</main>
       </body>
     </html>
