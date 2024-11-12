bbc2a69e9e83572dc9c1c5814976501f9db749ce
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-11-12 18:54:26 +0545

    feat: added title

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,16 @@ export default function Home() {
   return (
     <div className="content-wrapper ">
       <div className="conten">
-        {" "}
         <p
           style={{
             animationTimeline: "view()",
           }}
         >
-          Hi, there my name is Sandesh Thapa, a frontend developer, writer ,
-          react, nextjs enthusiast based in Kathmandu , Nepal.
-        </p>
-        <p>
-          I am currently working at{" "}
-          <a
-            target="_blank"
-            className="underline visited:text-red-400  text-green-700 "
-            rel="noreferrer"
-            href="https://aitc.ai/"
-          >
-             AITC international
-          </a>{" "}
-          as a Frontend Developer
+          Hi there, I’m Sandesh Thapa a software engineer specializing in web
+          development and frontend infrastructure. My passion is improving
+          Developer Experience (DX) by creating tools to make development
+          easier. I&apos;m currently working at AITC international full-time.
+          Here, I write about things I learn and find interesting.
         </p>
       </div>
     </div>
