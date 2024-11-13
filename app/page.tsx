2404e61b01eac565d55580c232341da83bb6c476
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-11-13 20:28:04 +0545

    feat: lint and prettier rules added

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,29 @@
+import ThemeChanger from './ThemeChanger';
+
 export default async function Home() {
   return (
-    <div className="content-wrapper flex justify-center items-center h-screen -mt-20 ">
-      <div className="size-full px-6 flex justify-center items-center py-6 md:px-8 md:py-12 md:max-w-screen-sm">
-        <div className="flex flex-row flex-wrap">
-          <p className="text-gray-500">
+    <div className='content-wrapper relative -mt-20 flex h-screen items-center justify-center'>
+      <div className='flex size-full items-center justify-center px-6 py-6 md:max-w-screen-sm md:px-8 md:py-12'>
+        <div className='flex flex-row flex-wrap'>
+          <p className='text-gray-500'>
             <span>Hi, there my name is </span>
-            <button className="hover:underline text-black ">
-              Sandesh Thapa
-            </button>{" "}
+            <button className='text-black hover:underline'>Sandesh Thapa</button>{' '}
             <span>
-              , a <span className="text-black">Frontend Developer</span> ,
-              writer , react, nextjs enthusiast based in Kathmandu , Nepal.
+              , a <span className='text-black'>Frontend Developer</span> , writer , react, nextjs enthusiast based in
+              Kathmandu , Nepal.
             </span>
             <span> I am currently working at </span>
             <a
-              target="_blank"
-              className="underline visited:text-red-400 p-1  text-green-700 "
-              rel="noreferrer"
-              href="https://aitc.ai/"
+              target='_blank'
+              className='p-1 text-green-700 underline visited:text-red-400'
+              rel='noreferrer'
+              href='https://aitc.ai/'
             >
-              AITC international{" "}
+              AITC international{' '}
             </a>
-            {"   "}
+            {'   '}
             <span>as a Frontend Developer</span>
+            <ThemeChanger />
           </p>
         </div>
       </div>
