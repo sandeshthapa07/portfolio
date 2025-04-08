8b663d125db8eff06e2134d4812587ebc074173f
Sandesh Thapa <sandesh.thapa@aitc.ai>
2025-04-08 14:40:48 +0545

    feat: toc created

--- app/blog/k/Toc.tsx
+++ app/blog/k/Toc.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+import { motion, useMotionValue, useTransform } from 'motion/react';
 
 const Toc = () => {
   const [headings, setHeadings] = useState<{ text: string; percentage: number }[]>([]);
@@ -14,7 +16,6 @@ const Toc = () => {
       return {
         text: heading.textContent?.trim() ?? '',
         bottom,
-        // percentage,
       };
     });
 
@@ -36,26 +37,45 @@ const Toc = () => {
     setHeadings(allhading);
   }, []);
 
+  const mouseY = useMotionValue(Infinity);
+  const ref = useRef<HTMLButtonElement>(null);
+  let width = useTransform(mouseY, [0, 300], [40, 80]);
+  console.log(mouseY);
+
   return (
-    <div className='fixed left-0 top-0 z-50 flex h-screen w-full flex-col items-start justify-start gap-2 overflow-y-scroll bg-white px-4 py-10'>
+    <motion.div
+      role='menuitem'
+      tabIndex={0}
+      aria-label='Table of contents'
+      onMouseOver={(e) => mouseY.set(e.pageY)}
+      onMouseLeave={(e) => mouseY.set(Infinity)}
+      className='fixed left-0 top-0 z-50 flex h-screen w-full flex-col items-start justify-start gap-2 overflow-y-scroll bg-white px-4 py-10'
+    >
       {headings.map((h) => (
-        <div key={h.text} className='flex flex-col gap-2'>
+        <div key={h.text} className='flex cursor-pointer flex-col gap-2'>
           {Array.from({ length: h.percentage }).map((_, lineIndex) => (
-            <div key={lineIndex + 1} className='flex flex-col gap-2'>
+            <motion.button
+              ref={ref}
+              style={{
+                width,
+              }}
+              key={lineIndex + 1}
+              className='flex flex-col gap-2'
+            >
               {lineIndex === 0 ? (
-                <button className='relative h-1 w-24 rounded bg-black'>
+                <span className='relative h-1 w-24 rounded bg-black'>
                   <span className='absolute left-full top-1/2 w-fit -translate-y-1/2 whitespace-nowrap pl-2 text-black'>
-                    {h.text} {h.percentage}
+                    {h.text}
                   </span>{' '}
-                </button>
+                </span>
               ) : (
                 <span className='h-1 w-16 rounded bg-black' />
               )}
-            </div>
+            </motion.button>
           ))}
         </div>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
