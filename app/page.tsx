1d34276a1830558429d719b26d1a281925abf481
sandeshThapa <suman.sandes@gmail.com>
2026-05-19 21:49:25 +0545

    feat: env changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export default async function Home() {
 
   return (
     <div className='content-wrapper relative -mt-20 flex h-screen items-center justify-center'>
       <div className='flex flex-col gap-1 md:max-w-screen-sm'>
-        <h1 className='text-lg font-medium'> Hello, I’m Sandesh Thapa</h1>
-
+        <h1 className='text-lg font-medium'> Hello, I’m {process.env.HOSTNAME}</h1>
+        <Link href="/blog">{process.env.NEXT_PUBLIC_SITE_URL}</Link>
         <p>
           I give <span className='text-green-500'>life</span> and <span className='text-red-500'>emotion</span> to the
           design{' '}
